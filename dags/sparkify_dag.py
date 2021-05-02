4975a2a41eae01d9fb0c2e95ea0a8ec136686895
Joël Atiamutu <joelatiam@googlemail.com>
2021-05-02 16:56:37 +0200

    feat: Implement S3 to Redshift Operator
    - Remove exemple dag
    - Update plugin name
    - Install configparser
    - Add New line at End of File

--- dags/sparkify_dag.py
+++ dags/sparkify_dag.py
@@ -1,20 +1,31 @@
+
+import configparser
 from datetime import datetime, timedelta
-import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+from operators import (StageToRedshiftOperator, LoadFactOperator,
                                 LoadDimensionOperator, DataQualityOperator)
 from helpers import SqlQueries
 
-# AWS_KEY = os.environ.get('AWS_KEY')
-# AWS_SECRET = os.environ.get('AWS_SECRET')
+config = configparser.ConfigParser()
+config.read('../ppl.cfg')
+
+S3_BUCKET = 'udacity-dend'
+S3_LOGS_PATH = 'log_data'
+S3_SONGS_PATH = 'song_data'
+
 
 default_args = {
-    'owner': 'udacity',
-    'start_date': datetime(2019, 1, 12),
+    'owner': 'joelatiam',
+    'start_date': datetime.now(),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False,
+    'email_on_retry': False
 }
 
-dag = DAG('udac_example_dag',
+dag = DAG('sparkify_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
           schedule_interval='0 * * * *'
@@ -24,11 +35,21 @@ start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    table="staging_events",
+    s3_bucket=S3_BUCKET,
+    s3_key=S3_LOGS_PATH,
     dag=dag
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    table="staging_songs",
+    s3_bucket=S3_BUCKET,
+    s3_key=S3_SONGS_PATH,
     dag=dag
 )
 
@@ -63,3 +84,13 @@ run_quality_checks = DataQualityOperator(
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+start_operator >> stage_events_to_redshift >> load_songplays_table
+start_operator >> stage_songs_to_redshift >> load_songplays_table
+
+load_songplays_table >> load_user_dimension_table >> run_quality_checks
+load_songplays_table >> load_song_dimension_table >> run_quality_checks
+load_songplays_table >> load_artist_dimension_table >> run_quality_checks
+load_songplays_table >> load_time_dimension_table >> run_quality_checks
+
+run_quality_checks >> end_operator
