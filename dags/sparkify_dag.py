cd38ac2342316beb8969c3937c86afbfa3c28232
Joël Atiamutu <joelatiam@googlemail.com>
2021-05-08 14:47:13 +0200

    test: Implement Data Quality Check
    - Refactor String quotes

--- dags/sparkify_dag.py
+++ dags/sparkify_dag.py
@@ -12,7 +12,6 @@ S3_LOGS_PATH = 'log_data'
 S3_LOG_JSONPATH='log_json_path.json'
 S3_SONGS_PATH = 'song_data'
 
-
 default_args = {
     'owner': 'joelatiam',
     'start_date': datetime(2021, 5, 3),
@@ -31,11 +30,13 @@ dag = DAG('sparkify_dag',
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
+records_to_check = []
+
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
-    redshift_conn_id="redshift",
-    aws_credentials_id="aws_credentials",
-    table="staging_events",
+    redshift_conn_id='redshift',
+    aws_credentials_id='aws_credentials',
+    table='staging_events',
     s3_bucket=S3_BUCKET,
     s3_key=S3_LOGS_PATH,
     s3_json_format=S3_LOG_JSONPATH,
@@ -44,9 +45,9 @@ stage_events_to_redshift = StageToRedshiftOperator(
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
-    redshift_conn_id="redshift",
-    aws_credentials_id="aws_credentials",
-    table="staging_songs",
+    redshift_conn_id='redshift',
+    aws_credentials_id='aws_credentials',
+    table='staging_songs',
     s3_bucket=S3_BUCKET,
     s3_key=S3_SONGS_PATH,
     dag=dag
@@ -54,46 +55,78 @@ stage_songs_to_redshift = StageToRedshiftOperator(
 
 load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
-    redshift_conn_id="redshift",
-    table="songplays",
+    redshift_conn_id='redshift',
+    table='songplays',
     select_query = SqlQueries.songplay_table_insert,
     dag=dag
 )
 
+records_to_check.append({
+    'table_name': 'songplays',
+    'min_records': 1,
+    'not_null_columns': ['playid']
+})
+
 load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
-    redshift_conn_id="redshift",
-    table="users",
+    redshift_conn_id='redshift',
+    table='users',
     select_query = SqlQueries.user_table_insert,
     dag=dag
 )
 
+records_to_check.append({
+    'table_name': 'users',
+    'min_records': 1,
+    'not_null_columns': ['userid']
+})
+
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
-    redshift_conn_id="redshift",
-    table="songs",
+    redshift_conn_id='redshift',
+    table='songs',
     select_query = SqlQueries.song_table_insert,
     dag=dag
 )
 
+records_to_check.append({
+    'table_name': 'songs',
+    'min_records': 1,
+    'not_null_columns': ['songid', 'title']
+})
+
 load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
-    redshift_conn_id="redshift",
-    table="artists",
+    redshift_conn_id='redshift',
+    table='artists',
     select_query = SqlQueries.artist_table_insert,
     dag=dag
 )
 
+records_to_check.append({
+    'table_name': 'artists',
+    'min_records': 1,
+    'not_null_columns': ['artistid', 'name']
+})
+
 load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
-    redshift_conn_id="redshift",
-    table="time",
+    redshift_conn_id='redshift',
+    table='time',
     select_query = SqlQueries.time_table_insert,
     dag=dag
 )
 
+records_to_check.append({
+    'table_name': 'time',
+    'min_records': 1,
+    'not_null_columns': ['start_time']
+})
+
 run_quality_checks = DataQualityOperator(
     task_id='Run_data_quality_checks',
+    redshift_conn_id='redshift',
+    tables_list = records_to_check,
     dag=dag
 )
 
